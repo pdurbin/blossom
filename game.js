d4c51827ab0938cb6170783ff43a1cd9b13abaa1
Philip Durbin <philipdurbin@gmail.com>
2017-07-30 21:10:48 -0400

    make more like flappy bird
    based on http://www.lessmilk.com/tutorial/flappy-bird-phaser-1

--- game.js
+++ game.js
@@ -1,121 +1,111 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
-    preload: preload,
-    create: create,
-    update: update
-});
+// based on http://www.lessmilk.com/tutorial/flappy-bird-phaser-1
+var game = new Phaser.Game(400, 490, Phaser.AUTO, "gameDiv");
 
-var tapMessage;
-var tapCount = 0;
-var winnerMessage;
-var debug;
-var bday;
-var cupcake;
-var pie;
-var gameover = 0;
+var mainState = {
 
-function preload() {
-    // to fit landscape on mobile
-    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    game.scale.pageAlignHorizontally = true;
-    game.scale.pageAlignVertically = true;
+    preload: function() { 
+        if(!game.device.desktop) {
+            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            //game.scale.setMinMax(game.width/2, game.height/2, game.width, game.height);
+            // to fit landscape on mobile
+            game.scale.pageAlignHorizontally = true;
+            game.scale.pageAlignVertically = true;
+        }
+        
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true;
 
-    game.load.image('splash', 'assets/blossom-draft.png');
-    game.load.image('background', 'assets/white.png');
-    game.load.image('cake', 'assets/cake.png');
-    game.load.image('cupcake', 'assets/cupcake.png');
-    game.load.image('pie', 'assets/pie.png');
-    //game.load.image('finishLine', 'assets/blackpixel.png');
-    game.load.image('finishLine', 'assets/rainbow.png');
+        game.stage.backgroundColor = '#29abe2';
 
-}
+        game.load.image('bird', 'assets/blossom.png');  
+        game.load.image('pipe', 'assets/cloud.png'); 
 
-function create() {
+    },
 
-    var splash = game.add.sprite(game.world.centerX, game.world.centerY, 'splash');
-    splash.anchor.setTo(0.5, 0.6);
-    splash.scale.setTo(0.5, 0.5);
+    create: function() { 
+        game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    //var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'background');
-    //logo.anchor.setTo(0.5, 0.5);
+        this.pipes = game.add.group();
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);           
 
-    var instructions = game.add.text(0, 0, 'In the game, Blossom starts out in the sky. If she hits a cloud, she falls into the ocean. If she hits some seaweed while she’s swimming in the ocean, she gets squirted onto the land. If she hits a rock or a boulder on land, then it’s game over. It’s like Flappy Bird, but you get three chances.', {
-        fill: 'black',
-        font: 'bold 18pt Arial',
-        wordWrap: 'true',
-        wordWrapWidth: 800,
-    });
+        this.bird = game.add.sprite(10, 245, 'bird');
+        this.bird.scale.setTo(0.1, 0.1);
+        game.physics.arcade.enable(this.bird);
+        this.bird.body.gravity.y = 1000; 
 
-    tapMessage = game.add.text(220, 0, '', {
-        fill: 'black'
-    });
+        // New anchor position
+        this.bird.anchor.setTo(-0.2, 0.5); 
+ 
+        var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        spaceKey.onDown.add(this.jump, this); 
+        game.input.onDown.add(this.jump, this);
 
-    winnerMessage = game.add.text(540, 0, '', {
-        fill: 'black'
-    });
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#000000" });  
 
-    debug = game.add.text(220, 220, '', {
-        fill: 'black'
-    });
+    },
 
-    /*
-    bday = game.add.sprite(0, 30, 'cake');
-    bday.scale.setTo(0.2, 0.2);
-    bday.inputEnabled = true;
-    bday.events.onInputDown.add(onTap, this);
-    game.physics.arcade.enable(bday);
+    update: function() {
+        if (this.bird.y < 0 || this.bird.y > game.world.height)
+            this.restartGame(); 
 
-    cupcake = game.add.sprite(0, 250, 'cupcake');
-    cupcake.scale.setTo(0.2, 0.2);
-    cupcake.inputEnabled = true;
-    cupcake.events.onInputDown.add(onTap, this);
-    game.physics.arcade.enable(cupcake);
+        game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this); 
+            
+        // Slowly rotate the bird downward, up to a certain point.
+        if (this.bird.angle < 20)
+            this.bird.angle += 1;  
+    },
 
-    pie = game.add.sprite(0, 400, 'pie');
-    pie.scale.setTo(0.2, 0.2);
-    pie.inputEnabled = true;
-    pie.events.onInputDown.add(onTap, this);
-    game.physics.arcade.enable(pie);
-    */
+    jump: function() {
+        // If the bird is dead, he can't jump
+        if (this.bird.alive == false)
+            return; 
 
-    lines = game.add.group();
-    lines.enableBody = true;
+        this.bird.body.velocity.y = -350;
 
-    /*
-    var finishLine = lines.create(750, 0, 'finishLine');
-    finishLine.scale.setTo(10, 800);
-    */
+        // Jump animation
+        game.add.tween(this.bird).to({angle: -20}, 100).start();
 
-}
+    },
 
-function update() {
-    game.physics.arcade.collide(bday, lines);
-    game.physics.arcade.collide(cupcake, lines);
-    game.physics.arcade.collide(pie, lines);
+    hitPipe: function() {
+        // If the bird has already hit a pipe, we have nothing to do
+        if (this.bird.alive == false)
+            return;
+            
+        // Set the alive property of the bird to false
+        this.bird.alive = false;
 
-    game.physics.arcade.overlap(bday, lines, reachFinish, null, this);
-    game.physics.arcade.overlap(cupcake, lines, reachFinish, null, this);
-    game.physics.arcade.overlap(pie, lines, reachFinish, null, this);
-}
+        // Prevent new pipes from appearing
+        game.time.events.remove(this.timer);
+    
+        // Go through all the pipes, and stop their movement
+        this.pipes.forEach(function(p){
+            p.body.velocity.x = 0;
+        }, this);
+    },
 
-function reachFinish(pastry, lines) {
-    if (gameover != 1) {
-        winnerMessage.text = pastry.key.toUpperCase() + " wins!";
-        gameover = 1;
-    }
-}
+    restartGame: function() {
+        game.state.start('main');
+    },
 
-// http://phaser.io/docs/2.6.2/Phaser.Events.html#onInputDown
-function onTap(pastry, pointer) {
-    tapCount++;
-    if (tapCount > 1) {
-        tapMessage.text = "You've tapped " + tapCount + " times!";
-    }
-    else {
-        tapMessage.text = "You've tapped " + tapCount + " time!";
-    }
-    if (gameover == 1) {
-        return;
-    }
-    pastry.x += 10;
-    //debug.text = pastry.frameName + " at " + pointer.position;
-}
+    addOnePipe: function(x, y) {
+        var pipe = game.add.sprite(x, y, 'pipe');
+        pipe.scale.setTo(0.2, 0.2);
+        this.pipes.add(pipe);
+        game.physics.arcade.enable(pipe);
+
+        pipe.body.velocity.x = -200;  
+        pipe.checkWorldBounds = true;
+        pipe.outOfBoundsKill = true;
+    },
+
+    addRowOfPipes: function() {
+        this.addOnePipe(400, 0 + (Math.random() * (400 - 100) + 100));   
+        this.labelScore.text = this.score;  
+        this.score += 1;
+    },
+};
+
+game.state.add('main', mainState);  
+game.state.start('main'); 
